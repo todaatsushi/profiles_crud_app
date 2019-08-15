86e07b915bcf9b2ac7d6c3407d38b94c651b43a8
Atsushi <todaatsushi0@gmail.com>
2019-08-15 14:32:38 +0100

    Pre migrations, add profile model extending default user and media folder for uploads

--- profiles/settings.py
+++ profiles/settings.py
@@ -2,6 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = '/'.join(BASE_DIR.split('/')[:-1])
 
 
 # Quick-start development settings - unsuitable for production
@@ -118,5 +119,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(SITE_ROOT, 'media')
