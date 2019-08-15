4db695925baf8b140845dc8dfaa786bae4b09cd4
Atsushi <todaatsushi0@gmail.com>
2019-08-15 15:47:44 +0100

    Implement manager for custom user model

---
+++ users/managers.py
@@ -0,0 +1,40 @@
+import uuid
+
+from django.contrib.auth.base_user import BaseUserManager
+from django.db.models import Q
+
+
+class UserManager(BaseUserManager):
+    """
+    Custom user model manager. No username field,
+    """
+    def create_user(self, email, password, **kwargs):
+        """
+        Create and then save base user instances given at least the email and password.
+        """
+        # Check args
+        if not email:
+            raise ValueError('Email must not be empty.')
+        if not password:
+            raise ValueError('Password must not be empty.')
+
+        # Formatting args
+        email = self.normalize_email(email)
+        user = self.model(email=email, **kwargs)
+        user.set_password(password)
+
+        user.save()
+        return user
+
+    def create_superuser(email, password, **kwargs):
+        """
+        Create and then save staff/super user instances given at least the email and password.
+        """
+        # If blank, set to True
+        kwargs.setdefault('is_superuser', True)
+
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('Superuser must be have is_superuser = True')
+
+        return self.create_user(email, passwor,d **kwargs)
+        
