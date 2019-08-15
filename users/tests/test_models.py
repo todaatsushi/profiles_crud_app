fc0b981b04b9b58f8c7ed2b407accc0248f65a3f
Atsushi <todaatsushi0@gmail.com>
2019-08-15 15:47:28 +0100

    Adjust tests for manager

--- users/tests/test_models.py
+++ users/tests/test_models.py
@@ -12,7 +12,7 @@ class UsersManagerTestCase(TestCase):
         user = User.objects.create_user(email='test@mail.com', password='test')
 
         self.assertEqual(user.email, 'test@mail.com')
-        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
 
         ## Custom User has no username
         try:
@@ -21,27 +21,27 @@ class UsersManagerTestCase(TestCase):
             pass
 
         # No args
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             User.objects.create_user()
 
         # invalid email, no password
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             User.objects.create_user(email='')
 
         # invalid email only
-        with self.assertRaises(TypeError):
+        with self.assertRaises(ValueError):
             User.objects.create_user(email='', password='valid')
 
-    def test_create_staff_user(self):
+    def test_create_superuser(self):
         """
         Using the custom UserManager, test if we can create a staff user with valid credentials.
         """
         # Create user
         User = get_user_model()
-        staff = User.objects.create_staff_user(email='test@mail.com', password='test')
+        staff = User.objects.create_superuser(email='test@mail.com', password='test')
 
         self.assertEqual(staff.email, 'test@mail.com')
-        self.assertTrue(staff.is_staff)
+        self.assertTrue(staff.is_superuser)
 
         ## Custom User has no username
         try:
@@ -51,14 +51,14 @@ class UsersManagerTestCase(TestCase):
 
         # Check method with invalid args
         # No args
-        with self.assertRaises(TypeError):
-            User.objects.create_staff_user()
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser()
 
         # invalid email, no password
-        with self.assertRaises(TypeError):
-            User.objects.create_staff_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(email='')
 
         # invalid email only
-        with self.assertRaises(TypeError):
-            User.objects.create_staff_user(email='', password='valid')
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(email='', password='valid')
 
