597aaf12d1ee5111bd8b3c956297cba06cc90a4b
Atsushi <todaatsushi0@gmail.com>
2019-08-15 20:14:46 +0100

    Implement creation and update forms for admin page

---
+++ users/forms.py
@@ -0,0 +1,23 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from users.models import BaseUser
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm):
+        model = BaseUser
+        fields = [
+            'email', 'first_name', 'last_name',
+            'about', 'company', 'role',
+            'responsibilities',
+        ]
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm):
+        model = BaseUser
+        fields = [
+            'email', 'first_name', 'last_name',
+            'about', 'company', 'role',
+            'responsibilities',
+        ]
