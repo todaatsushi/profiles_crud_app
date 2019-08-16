8e2b2a31b293d2e03b932eea70f89d4ef8649afb
Atsushi <todaatsushi0@gmail.com>
2019-08-16 17:44:20 +0100

    Add testing framework for views

---
+++ users/tests/test_views.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+
+
+class UsersViewsTestCase(TestCase):
+    def test_all_base_users_view_template(self):
+        pass
+
+    def test_base_user_detail_view_template(self):
+        pass
+
+    def test_base_user_create_view_authentication(self):
+        pass
+
+    def test_base_user_create_view_template(self):
+        pass
+
+    def test_base_user_update_view_authentication(self):
+        pass
+
+    def test_base_user_update_view_template(self):
+        pass
+    
+    def test_base_user_delete_view_authentication(self):
+        pass
+
+    def test_base_user_delete_view_template(self):
+        pass
