de304c926cdcfdf65f4d6f58cb90d9d1eb6f36a6
Atsushi <todaatsushi0@gmail.com>
2019-08-17 17:27:50 +0100

    Add urls for login and logout

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as av
 
 import users.views as v
 
 
 urlpatterns = [
     path('', v.AllBaseUsersView.as_view(), name='user-list'),
+
+    # Login/Logout views
+    path('user/login/', av.LoginView.as_view(template_name='users/user_login.html'), name='user-login'),
+    path('user/logout/', av.LogoutView.as_view(), name='user-logout'),
+
     path('user/new/', v.BaseUserCreateView.as_view(), name='user-create'),
     path('user/<str:pk>/', v.BaseUserDetailView.as_view(), name='user-detail'),
     path('user/<str:pk>/update/', v.BaseUserUpdateView.as_view(), name='user-update'),
