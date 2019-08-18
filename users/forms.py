471109a67cc3d3fe3c26d5d04cc06b31efe459db
Atsushi <todaatsushi0@gmail.com>
2019-08-18 17:47:48 +0100

    Remove unused form

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ import django.contrib.auth.forms as auth_forms
 
 from users.models import BaseUser
 
+
 # Registration/Update Form
 class BaseUserCreateForm(auth_forms.UserCreationForm):
     class Meta(auth_forms.UserCreationForm):
@@ -20,30 +21,3 @@ class BaseUserUpdateForm(auth_forms.UserChangeForm):
             'email', 'first_name', 'last_name',
             'about', 'company', 'role', 'responsibilities'
         ]
-
-
-# class BaseUserUpdateForm(auth_forms.UserChangeForm):
-#     password = forms.CharField(widget=forms.PasswordInput())
-#     confirm_password = forms.CharField(widget=forms.PasswordInput())
-
-#     class Meta(auth_forms.UserChangeForm):
-#         model = BaseUser
-#         fields = [
-#             'email', 'first_name', 'last_name',
-#             'about', 'company', 'role', 'responsibilities'
-#         ]
-
-#     def clean(self):
-#         """
-#         Make sure password and confirm password are matching.
-#         """
-#         cleaned = super().clean()
-#         password = cleaned.get('password')
-#         confirm = cleaned.get('confirm_password')
-
-#         if password != confirm:
-#             raise forms.ValidationError(
-#                 "Your passwords don't match! Please enter them again."
-#             )
-
-#         return cleaned
