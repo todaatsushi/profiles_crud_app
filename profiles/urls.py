27f720f7b5fde375a8fad6906299409bcf2b9ccc
Atsushi <todaatsushi0@gmail.com>
2019-08-18 19:10:13 +0100

    Add password change urls to main tree

--- profiles/urls.py
+++ profiles/urls.py
@@ -14,6 +14,15 @@ urlpatterns = [
          name='user-login'),
     path('logout/', av.LogoutView.as_view(), name='user-logout'),
 
+    # Password reset
+    path('password-change/',
+            av.PasswordChangeView.as_view(template_name='users/user_password_change.html'), name='password_change'
+    ),
+    path('password-change/done/',
+            av.PasswordChangeDoneView.as_view(template_name='users/user_password_change_done.html'),
+            name='password_change_done'
+    ),
+
     # All auth
     path('auth/', include('allauth.urls')),
 ]
