03a2292dbdbbd1bf6d73aa283a9a023584db7fb5
Atsushi <todaatsushi0@gmail.com>
2019-08-18 17:59:25 +0100

    Make urls easier to understand

--- users/urls.py
+++ users/urls.py
@@ -9,21 +9,16 @@ urlpatterns = [
     path('', v.AllBaseUsersView.as_view(), name='user-list'),
 
     # Profile paths
-    path('user/new/', v.BaseUserCreateView.as_view(), name='user-create'),
-    path('user/<str:slug>/', v.BaseUserDetailView.as_view(), name='user-detail'),
-    path('user/<str:slug>/update/', v.BaseUserUpdateView.as_view(), name='user-update'),
-    path('user/<str:slug>/delete/', v.BaseUserDeleteView.as_view(), name='user-delete'),
-
-    # Login/Logout views
-    path('login/', av.LoginView.as_view(template_name='users/user_login.html'),
-         name='user-login'),
-    path('logout/', av.LogoutView.as_view(), name='user-logout'),
+    path('new/', v.BaseUserCreateView.as_view(), name='user-create'),
+    path('<str:slug>/', v.BaseUserDetailView.as_view(), name='user-detail'),
+    path('<str:slug>/update/', v.BaseUserUpdateView.as_view(), name='user-update'),
+    path('<str:slug>/delete/', v.BaseUserDeleteView.as_view(), name='user-delete'),
 
     # Password reset
-    path('user/password-change/',
+    path('password-change/',
             av.PasswordChangeView.as_view(template_name='users/password_change.html'), name='password_change'
     ),
-    path('user/password-change/done/',
+    path('password-change/done/',
             av.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
             name='password_change_done'
     ),
