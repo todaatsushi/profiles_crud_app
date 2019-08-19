b00ba2a4cdb0bffd06624980604bfbc8ae1139c7
Atsushi <todaatsushi0@gmail.com>
2019-08-19 15:21:05 +0100

    Add assumption of .env file existing for different envs. Change settings accordingly and update packages

--- profiles/settings.py
+++ profiles/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3j1dp%_j69ta&aa#9rf1oh)ih+a8wm%43shelkjahc90e^zwpn'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', True) == True
 
 ALLOWED_HOSTS = ['*'] if DEBUG else []
 
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'profiles.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-DOCKER = True
+DOCKER = os.environ.get('DOCKER', False) == 'True'
 
 if DOCKER:
     DATABASES = {
