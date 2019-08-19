a4b49e46ab48b11e179077822d78a083d03ed083
Atsushi <todaatsushi0@gmail.com>
2019-08-19 16:29:21 +0100

    Fix admin and allow sign in with github

--- users/admin.py
+++ users/admin.py
@@ -5,11 +5,12 @@ from users.models import BaseUser
 
 class BaseUserAdmin(admin.ModelAdmin):
     list_display = (
-        'email', 'is_staff', 'first_name', 'last_name',
+        'username', 'email', 'is_staff', 'first_name', 'last_name',
         'about', 'company', 'role',
     )
     fields = [
         ('first_name', 'last_name', 'email'),
+        ('username', 'password'),
         'is_staff', 'about',
         ('company', 'role'),
         'responsibilities',
