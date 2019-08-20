1e5307062f59c53d90e7f350ef0f6d8b47357e08
Atsushi <todaatsushi0@gmail.com>
2019-08-20 23:45:28 +0100

    Add all hosts to allow Docker testing to pass

--- profiles/settings.py
+++ profiles/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True) == True
 
-ALLOWED_HOSTS = ['*'] if DEBUG else ['localhost']
+ALLOWED_HOSTS = ['*'] if DEBUG else ['*']
 
 
 # Application definition
